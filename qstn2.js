fd531c69440a394abb82b72a59ba7e5b5997e969
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-17 20:30:21 +0530

    second done

---
+++ qstn2.js
@@ -0,0 +1,15 @@
+let number = 15;
+
+if (number % 3 === 0) {
+  if (number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else {
+    console.log("Fizz");
+  }
+} else {
+  if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log("Not FizzBuzz");
+  }
+}
