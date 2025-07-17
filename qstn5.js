c111e56eb5a7eedeaabfad5f29c0b0aa508494a8
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-17 20:35:20 +0530

    fifth done

---
+++ qstn5.js
@@ -0,0 +1,15 @@
+let value = 144;
+
+if (value % 2 === 0) {
+  if (value > 100) {
+    console.log("Even and greater than 100");
+  } else {
+    console.log("Even and less than or equal to 100");
+  }
+} else {
+  if (value > 100) {
+    console.log("Odd and greater than 100");
+  } else {
+    console.log("Odd and less than or equal to 100");
+  }
+}
